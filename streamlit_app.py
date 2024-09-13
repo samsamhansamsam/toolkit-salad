a5eaaeb574b4ea049ea302453fcb8bcc3394de48
samsamhansamsam <125422309+samsamhansamsam@users.noreply.github.com>
2024-09-13 07:53:46 +0000

    뉴 객단가 도구

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
+# app.py
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title='샐러드랩 툴킷', page_icon='🚀')
+
+st.title('샐러드랩 툴킷')
+st.write('왼편의 사이드바를 통해 원하는 툴킷 활용 가능')
+st.write('기타 문의나 개선사항 전달은 재윤님에게 전달')
