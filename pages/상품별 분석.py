20753e52753ac6d7256d2287523f442839cd7385
samsamhansamsam <125422309+samsamhansamsam@users.noreply.github.com>
2024-10-25 01:45:22 +0000

    한글 폰트 2

--- pages/상품별 분석.py
+++ pages/상품별 분석.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import itertools
 
+plt.rcParams['font.family'] = 'NanumGothic'
+
 def run_product_analysis():
     st.title('상품 연관성 분석')
 
