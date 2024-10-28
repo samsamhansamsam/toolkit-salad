99cef202c49b789022fe4cd5641ebe7c86a81192
samsamhansamsam <125422309+samsamhansamsam@users.noreply.github.com>
2024-10-28 01:23:56 +0000

    문구 수정

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.set_page_config(page_title='샐러드랩 툴킷', page_icon='🚀')
 
 st.title('샐러드랩 툴킷')
 st.write('왼편의 사이드바를 통해 원하는 툴킷 활용 가능')
-st.write('기타 문의나 개선사항 전달은 재윤님에게 전달')
+st.write('기타 문의나 개선사항은 재윤님에게 전달')
